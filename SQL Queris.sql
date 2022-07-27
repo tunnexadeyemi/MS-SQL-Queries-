20beeede9c8d40d0a758276d23c1e1a809ce301d
AmoleTheAnalyst <63958271+tunnexadeyemi@users.noreply.github.com>
2022-07-27 14:10:55 -0400

    Add files via upload

---
+++ SQL Queris.sql
@@ -0,0 +1,304 @@
+/*
+
+
+Table 1 Query:
+Create Table EmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Table 2 Query:
+Create Table EmployeeSalary 
+(EmployeeID int, 
+JobTitle varchar(50), 
+Salary int
+)
+
+
+
+Table 1 Insert:
+Insert into EmployeeDemographics VALUES
+(1001, 'Jim', 'Halpert', 30, 'Male'),
+(1002, 'Pam', 'Beasley', 30, 'Female'),
+(1003, 'Dwight', 'Schrute', 29, 'Male'),
+(1004, 'Angela', 'Martin', 31, 'Female'),
+(1005, 'Toby', 'Flenderson', 32, 'Male'),
+(1006, 'Michael', 'Scott', 35, 'Male'),
+(1007, 'Meredith', 'Palmer', 32, 'Female'),
+(1008, 'Stanley', 'Hudson', 38, 'Male'),
+(1009, 'Kevin', 'Malone', 31, 'Male')
+
+Table 2 Insert:
+Insert Into EmployeeSalary VALUES
+(1001, 'Salesman', 45000),
+(1002, 'Receptionist', 36000),
+(1003, 'Salesman', 63000),
+(1004, 'Accountant', 47000),
+(1005, 'HR', 50000),
+(1006, 'Regional Manager', 65000),
+(1007, 'Supplier Relations', 41000),
+(1008, 'Salesman', 48000),
+(1009, 'Accountant', 42000) */
+
+
+
+
+/*
+Table 1 Insert:
+Insert into EmployeeDemographics VALUES
+(1011, 'Ryan', 'Howard', 26, 'Male'),
+(NULL, 'Holly', 'Flax', NULL, NULL),
+(1013, 'Darryl', 'Philbin', NULL, 'Male')
+
+Table 3 Query:
+Create Table WareHouseEmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Table 3 Insert:
+Insert into WareHouseEmployeeDemographics VALUES
+(1013, 'Darryl', 'Philbin', NULL, 'Male'),
+(1050, 'Roy', 'Anderson', 31, 'Male'),
+(1051, 'Hidetoshi', 'Hasagawa', 40, 'Male'),
+(1052, 'Val', 'Johnson', 31, 'Female')
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+
+*/
+
+
+/*
+Stored Procedures.sql
+
+
+CREATE PROCEDURE Temp_Employee
+AS
+DROP TABLE IF EXISTS #temp_employee
+Create table #temp_employee (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
+
+
+Insert into #temp_employee
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM SQLTutorial..EmployeeDemographics emp
+JOIN SQLTutorial..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle
+
+Select * 
+From #temp_employee
+GO;
+
+
+
+
+CREATE PROCEDURE Temp_Employee2 
+@JobTitle nvarchar(100)
+AS
+DROP TABLE IF EXISTS #temp_employee3
+Create table #temp_employee3 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
+
+
+Insert into #temp_employee3
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM SQLTutorial..EmployeeDemographics emp
+JOIN SQLTutorial..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+where JobTitle = @JobTitle --- make sure to change this in this script from original above
+group by JobTitle
+
+Select * 
+From #temp_employee3
+GO;
+
+
+exec Temp_Employee2 @jobtitle = 'Salesman'
+exec Temp_Employee2 @jobtitle = 'Accountant'
+
+
+
+*/
+
+
+/*
+
+/*
+String Functions
+Today's Topic: String Functions - TRIM, LTRIM, RTRIM, Replace, Substring, Upper, Lower
+
+
+
+--Drop Table EmployeeErrors;
+
+
+CREATE TABLE EmployeeErrors (
+EmployeeID varchar(50)
+,FirstName varchar(50)
+,LastName varchar(50)
+)
+
+Insert into EmployeeErrors Values 
+('1001  ', 'Jimbo', 'Halbert')
+,('  1002', 'Pamela', 'Beasely')
+,('1005', 'TOby', 'Flenderson - Fired')
+
+Select *
+From EmployeeErrors
+
+-- Using Trim, LTRIM, RTRIM
+
+Select EmployeeID, TRIM(employeeID) AS IDTRIM
+FROM EmployeeErrors 
+
+Select EmployeeID, RTRIM(employeeID) as IDRTRIM
+FROM EmployeeErrors 
+
+Select EmployeeID, LTRIM(employeeID) as IDLTRIM
+FROM EmployeeErrors 
+
+	
+
+
+
+-- Using Replace
+
+Select LastName, REPLACE(LastName, '- Fired', '') as LastNameFixed
+FROM EmployeeErrors
+
+
+-- Using Substring
+
+Select Substring(err.FirstName,1,3), Substring(dem.FirstName,1,3), Substring(err.LastName,1,3), Substring(dem.LastName,1,3)
+FROM EmployeeErrors err
+JOIN EmployeeDemographics dem
+	on Substring(err.FirstName,1,3) = Substring(dem.FirstName,1,3)
+	and Substring(err.LastName,1,3) = Substring(dem.LastName,1,3)
+
+
+
+-- Using UPPER and lower
+
+Select firstname, LOWER(firstname)
+from EmployeeErrors
+
+Select Firstname, UPPER(FirstName)
+from EmployeeErrors
+
+*/
+
+
+/*
+
+
+
+
+
+ Subqueries (in the Select, From, and Where Statement)
+*/
+
+Select EmployeeID, JobTitle, Salary
+From EmployeeSalary
+
+-- Subquery in Select
+
+Select EmployeeID, Salary, (Select AVG(Salary) From EmployeeSalary) as AllAvgSalary
+From EmployeeSalary
+
+-- How to do it with Partition By
+Select EmployeeID, Salary, AVG(Salary) over () as AllAvgSalary
+From EmployeeSalary
+
+-- Why Group By doesn't work
+Select EmployeeID, Salary, AVG(Salary) as AllAvgSalary
+From EmployeeSalary
+Group By EmployeeID, Salary
+order by EmployeeID
+
+
+-- Subquery in From
+
+Select a.EmployeeID, AllAvgSalary
+From 
+	(Select EmployeeID, Salary, AVG(Salary) over () as AllAvgSalary
+	 From EmployeeSalary) a
+Order by a.EmployeeID
+
+
+-- Subquery in Where
+
+
+Select EmployeeID, JobTitle, Salary
+From EmployeeSalary
+where EmployeeID in (
+	Select EmployeeID 
+	From EmployeeDemographics
+	where Age > 30)
+
+*/
+
+
+
+/*
+TEMP  TABLES
+Create table #temp_employee2 (
+EmployeeID int,
+JobTitle varchar(100),
+Salary int
+)
+
+Select * From #temp_employee2
+
+Insert into #temp_employee2 values (
+'1001', 'HR', '45000'
+)
+
+Insert into #temp_employee2 
+SELECT * From SQLTutorial..EmployeeSalary
+
+Select * From #temp_employee2
+
+
+
+
+DROP TABLE IF EXISTS #temp_employee3
+Create table #temp_employee3 (
+JobTitle varchar(100),
+EmployeesPerJob int ,
+AvgAge int,
+AvgSalary int
+)
+
+
+Insert into #temp_employee3
+SELECT JobTitle, Count(JobTitle), Avg(Age), AVG(salary)
+FROM SQLTutorial..EmployeeDemographics emp
+JOIN SQLTutorial..EmployeeSalary sal
+	ON emp.EmployeeID = sal.EmployeeID
+group by JobTitle
+
+Select * 
+From #temp_employee3
+
+SELECT AvgAge * AvgSalary
+from #temp_employee3
+*/
